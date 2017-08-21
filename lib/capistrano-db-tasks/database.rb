ee47909269879e69cf10ac24bc368f5f3bc76e1d
Oleg Bavaev <csolg7@gmail.com>
2017-08-22 04:01:10 +0500

    Fix run without rvm

--- lib/capistrano-db-tasks/database.rb
+++ lib/capistrano-db-tasks/database.rb
@@ -108,10 +108,9 @@ module Database
       puts "Loading remote database config"
       @cap.within @cap.current_path do
         @cap.with rails_env: @cap.fetch(:rails_env) do
-          dirty_config_content = @cap.capture(:rails, "runner \"puts '#{DBCONFIG_BEGIN_FLAG}' + ActiveRecord::Base.connection.instance_variable_get(:@config).to_yaml + '#{DBCONFIG_END_FLAG}'\"", '2>/dev/null')
-          # Remove all warnings, errors and artefacts produced by bunlder, rails and other useful tools
-          config_content = dirty_config_content.match(/#{DBCONFIG_BEGIN_FLAG}(.*?)#{DBCONFIG_END_FLAG}/m)[1]
-          @config = YAML.load(config_content).each_with_object({}) { |(k, v), h| h[k.to_s] = v }
+          config_content = @cap.capture(:cat, "config/database.yml", '2>/dev/null')
+          full_config = YAML.load(config_content).each_with_object({}) { |(k, v), h| h[k.to_s] = v }
+          @config = full_config[fetch(:rails_env)]
         end
       end
     end
